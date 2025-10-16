f911a86c7aa9c5001d0ad2cc5ede5ff2252ada4e
likerhythm <wjdalsdl0412@gmail.com>
2025-10-16 17:57:36 +0900

    fix(input-parser): 커스텀 구분자가 숫자인 경우 예외 반환

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -19,7 +19,24 @@ public class InputParser {
         Matcher matcher = CUSTOM_PREFIX.matcher(s);
         if (matcher.find()) { // 커스텀 구분자가 존재하는 경우
             String customDelimiter = matcher.group(1);
-            if (customDelimiter.length() != 1) {
+            validateCustomDelimiter(customDelimiter);
+            delimiters.add(Pattern.quote(customDelimiter));
+
+            return validateNumberFormat(s, 4);
+        }
+        return validateNumberFormat(s, 0);
+    }
+
+    private void validateCustomDelimiter(String customDelimiter) {
+        if (customDelimiter.length() != 1) { // 커스텀 구분자의 길이가 1이 아닌 경우
+            throw new IllegalArgumentException();
+        }
+        char c = customDelimiter.charAt(0);
+        if (Character.isDigit(c)) { // 커스텀 구분자가 숫자인 경우
+            throw new IllegalArgumentException();
+        }
+    }
+
                 throw new IllegalArgumentException();
             }
             delimiters.add(Pattern.quote(customDelimiter));
