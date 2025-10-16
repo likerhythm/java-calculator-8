e4083d2d7af5778e6e721aa902cc56f363d7ce83
likerhythm <wjdalsdl0412@gmail.com>
2025-10-16 17:01:45 +0900

    fix(input-parser): 커스텀 구분자 길이가 1이 아닌 경우 예외 반환

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class InputParser {
 
-    private static final Pattern CUSTOM_PREFIX = Pattern.compile("^//(.)\\n");
+    private static final Pattern CUSTOM_PREFIX = Pattern.compile("^//(.*)\\n");
     private static Set<String> delimiters = new HashSet<>();
 
     public InputParser() {
@@ -19,8 +19,11 @@ public class InputParser {
         Matcher matcher = CUSTOM_PREFIX.matcher(s);
         if (matcher.find()) { // 커스텀 구분자가 존재하는 경우
             String customDelimiter = matcher.group(1);
+            if (customDelimiter.length() != 1) {
+                throw new IllegalArgumentException();
+            }
             delimiters.add(Pattern.quote(customDelimiter));
-            return s.substring(5).split(String.join("|", delimiters));
+            return s.substring(4).split(String.join("|", delimiters));
         }
 
         return s.split(String.join("|", delimiters));
