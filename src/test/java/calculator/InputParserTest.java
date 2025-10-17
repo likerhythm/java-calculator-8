8c0359d009b0be01c05945758fc8df4bd710bfa6
likerhythm <wjdalsdl0412@gmail.com>
2025-10-17 17:09:50 +0900

    test(input-parser): 입력값 파싱 작업 테스트

---
+++ src/test/java/calculator/InputParserTest.java
@@ -0,0 +1,83 @@
+package calculator;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputParserTest {
+
+    InputParser inputParser = new InputParser();
+
+    @AfterEach
+    void afterEach() {
+        inputParser = new InputParser();
+    }
+
+    @Test
+    void 기본_파싱_작업이_정상적으로_작동합니다() {
+        String[] parsed = inputParser.parse("1:2:3");
+        Assertions.assertArrayEquals(new String[] {"1", "2", "3"}, parsed);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1:2;3,4", "// \n1 2:3,4"})
+    void 커스텀_구분자를_포함한_파싱_작업이_정상적으로_동작합니다(String v) {
+        String[] parsed = inputParser.parse(v);
+        Assertions.assertArrayEquals(new String[] {"1", "2", "3", "4"}, parsed);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"// \n1 2;3,4"})
+    void 등록되지_않은_구분자가_포함된_경우_예외가_발생합니다(String v) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse(v));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"  ", ";;"})
+    void 커스텀_구분자의_길이가_2_이상인_경우_예외가_발생합니다(String v) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse("//" + v + "\n1:2;3,4"));
+    }
+
+    @Test
+    void 커스텀_구분자의_길이가_0인_경우_예외가_발생합니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse("//\n1:2;3,4"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+    void 커스텀_구분자가_숫자인_경우_예외가_발생합니다(int v) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse("//" + v + "\n1:2;3,4"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;n1;2", "//;\1;2", "/;\n1;2", ";\n1;2", "//;1;2", "/;n1;2", "/;\1;2", "/1;\n1;2", "1/;\n1;2"})
+    void 커스텀_구분자를_잘못_표현한_경우_예외가_발생합니다(String v) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse(v));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n", ""})
+    void 빈_문자열인_경우_0을_반환합니다(String v) {
+        String[] parsed = inputParser.parse(v);
+        Assertions.assertArrayEquals(new String[] {"0"}, parsed);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2.4:3.5", "//;\n2.4:3.5"})
+    void 소수점이_포함된_수를_정상적으로_파싱합니다(String v) {
+        String[] parsed = inputParser.parse(v);
+        Assertions.assertArrayEquals(new String[] {"2.4", "3.5"}, parsed);
+    }
+
+    @Test
+    void 온점은_커스텀_구분자로_사용할_수_없습니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse("//.\n2:4"));
+    }
+
+    @Test
+    void 하이픈은_커스텀_구분자로_사용할_수_없습니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> inputParser.parse("//-\n2:4"));
+    }
+}
