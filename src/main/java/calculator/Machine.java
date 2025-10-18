bcf75a78ac584b559f80842b0f45c989054b5446
likerhythm <wjdalsdl0412@gmail.com>
2025-10-18 15:07:03 +0900

    feat(machine): 실제 동작을 수행하는 클래스 구현

---
+++ src/main/java/calculator/Machine.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Machine {
+
+    private Brain brain;
+    private InputParser inputParser;
+
+    public Machine() {
+        this.brain = new Brain();
+        this.inputParser = new InputParser();
+    }
+
+    public void run() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String line = Console.readLine();
+        String[] parsed = inputParser.parse(line);
+        String result = brain.plus(parsed);
+        System.out.println("결과 : " + result);
+    }
+}
