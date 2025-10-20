689ba62b12726f4abf7cbeda1df0ff7028bbe0b9
likerhythm <wjdalsdl0412@gmail.com>
2025-10-20 22:17:28 +0900

    test: 정수와 소수가 섞인 연산 테스트

--- src/test/java/calculator/BrainTest.java
+++ src/test/java/calculator/BrainTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,10 @@ public class BrainTest {
         String a = "100000000000000000000000000"; // 10조 * 10조
         String b = "100000000000000000000000000";
         Assertions.assertEquals("200000000000000000000000000", brain.run(new PostFix(a + " " + b + " +", true)));
+    }
+
+    @Test
+    void 정수와_소수가_섞인_연산을_정상적으로_수행합니다() {
+        Assertions.assertEquals("1.2", brain.run(new PostFix("1 0.2 +", true)));
     }
 }
