69a69dd6b24af9e43f8399375ca263b9312cefd9
likerhythm <wjdalsdl0412@gmail.com>
2025-10-20 17:59:56 +0900

    refactor(operator): 계산을 operator에게 위임한 것을 적용

--- src/main/java/calculator/Brain.java
+++ src/main/java/calculator/Brain.java
@@ -1,7 +1,7 @@
 package calculator;
 
+import calculator.operator.Operators;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Stack;
 
 public class Brain {
@@ -14,74 +14,39 @@ public class Brain {
         return String.valueOf(calculate(postfix.getValue()));
     }
 
-    public static double calculate(String postfix) {
+    private double calculate(String postfix) {
         Stack<Double> stack = new Stack<>();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < postfix.length(); i++) {
-            char c = postfix.charAt(i);
-
-            if (Character.isDigit(c) || c == '.') {
-                sb.append(c);
-                if (i + 1 >= postfix.length()) {
-                    stack.push(Double.parseDouble(sb.toString()));
-                }
-            } else if(c == ' ') {
-                stack.push(Double.parseDouble(sb.toString()));
-                sb = new StringBuilder();
-            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+        String[] tokens = postfix.split(" ");
+        for (String token : tokens) {
+            if (Operators.isOperator(token)) {
                 double operand2 = stack.pop();
                 double operand1 = stack.pop();
 
                 double result = 0;
-                if (c == '+') {
-                    result = operand1 + operand2;
-                } else if (c == '-') {
-                    result = operand1 - operand2;
-                } else if (c == '*') {
-                    result = operand1 * operand2;
-                } else if (c == '/') {
-                    result = operand1 / operand2;
-                }
+                result = Operators.calc(operand1, operand2, token);
 
                 stack.push(result);
+            } else {
+                stack.push(Double.parseDouble(token));
             }
         }
 
         return stack.pop();
     }
 
-    public static BigDecimal calculateBigDecimal(String postfix) {
+    private BigDecimal calculateBigDecimal(String postfix) {
         Stack<BigDecimal> stack = new Stack<>();
         String[] tokens = postfix.split(" ");
 
-        StringBuilder sb = new StringBuilder();
         for (String token : tokens) {
             if (token.isEmpty()) {
                 continue;
             }
 
-            if (token.equals("+") || token.equals("-") ||
-                    token.equals("*") || token.equals("/")) {
+            if (Operators.isOperator(token)) {
                 BigDecimal operand2 = stack.pop();
                 BigDecimal operand1 = stack.pop();
-
-                BigDecimal result = null;
-                switch (token) {
-                    case "+":
-                        result = operand1.add(operand2);
-                        break;
-                    case "-":
-                        result = operand1.subtract(operand2);
-                        break;
-                    case "*":
-                        result = operand1.multiply(operand2);
-                        break;
-                    case "/":
-                        result = operand1.divide(operand2, 10, RoundingMode.HALF_UP);
-                        break;
-                }
-
+                BigDecimal result = Operators.calc(operand1, operand2, token);
                 stack.push(result);
             }
             else {
