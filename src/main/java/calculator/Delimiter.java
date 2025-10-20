54e71bc2d9ed88169cc9bad4b7f41088d2fffd97
likerhythm <wjdalsdl0412@gmail.com>
2025-10-20 15:12:48 +0900

    feat(delimiter): 구분자 관리 클래스 추가

---
+++ src/main/java/calculator/Delimiter.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Delimiter {
+
+    private final char value;
+    private final Operator operator;
+
+    public Delimiter(char value, Operator operator) {
+        this.value = value;
+        this.operator = operator;
+    }
+
+    public char getOperatorCharacter() {
+        return operator.getCharacter();
+    }
+
+    public boolean equals(char target) {
+        return value ==  target;
+    }
+
+    public char getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/calculator/Delimiters.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Delimiters {
+
+    private List<Delimiter> delimiters;
+
+    public Delimiters() {
+        this.delimiters = new ArrayList<>();
+        this.delimiters.add(new Delimiter(',', Operators.getOperator(Plus.class)));
+        this.delimiters.add(new Delimiter(':', Operators.getOperator(Plus.class)));
+    }
+
+    public char delimiterToOperator(char c) {
+        Delimiter delimiter = delimiters.stream().filter(d -> d.equals(c))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 구분자입니다"));
+        return delimiter.getValue();
+    }
+
+    public void add(char c) {
+        this.delimiters.add(new Delimiter(c, Operators.getOperator(Plus.class)));
+    }
+
+    public String[] split(String s, int beginIndex) {
+        String[] cs = delimiters.stream()
+                .map(d -> "\\" + d.getValue())
+                .toArray(String[]::new);
+        return s.substring(beginIndex).split(String.join("|", cs));
+    }
+}
